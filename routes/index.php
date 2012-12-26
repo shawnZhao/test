47998c35f88eb08b4b408697de2e1108b66cb24e
shawnZhao <goooooogle.zsy@gmail.com>
2012-12-26 21:52:19 +0800

    Change-Id: I9b060b217a443a7015b715504ee65a593916dd1b

--- routes/index.php
+++ routes/index.php
@@ -1,9 +1,21 @@
 <?php
-include_once './vendor/autoload.php';
-$app = new \Slim\Slim();
+$app = new \Slim\Slim(array(
+    'templates.path' => './templates',
+	'view' => new \Slim\Extras\Views\Twig() //TODO
+));
+
+// $loader = new Twig_Loader_Filesystem('./templates');
+// $twig = new Twig_Environment($loader, array(
+// 		'cache' => './templates_cache',
+// ));
+
 
 $app->get('/', function () {
 	echo 'index request';
+});
+
+$app->get('/welcome', function () use ($app) {
+	echo $app->render('welcome.html', array('name' => 'shawn'));
 });
 
 $app->get('/home/:args', function ($args = 'a') {
