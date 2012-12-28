3b2deabafc656becba6bf4d42edaaa1db6d1132d
shawnZhao <goooooogle.zsy@gmail.com>
2012-12-28 16:41:08 +0800

    Change-Id: If3d1961c7c2f44f2b339f1d091bdf2a31fcdd4bd

--- index.php
+++ index.php
@@ -3,7 +3,12 @@ include_once './vendor/autoload.php';
 
 mb_internal_encoding ( "UTF-8" );
 date_default_timezone_set ( 'Asia/Shanghai' );
- 
+
+$app = new \Slim\Slim(array(
+		'templates.path' => './templates',
+		'view' => new \Slim\Extras\Views\Twig() //TODO
+));
+
 include_once './routes/index.php';
 
 $app->run();
--- routes/index.php
+++ routes/index.php
@@ -1,23 +1,24 @@
 <?php
-$app = new \Slim\Slim(array(
-    'templates.path' => './templates',
-	'view' => new \Slim\Extras\Views\Twig() //TODO
-));
 
 // $loader = new Twig_Loader_Filesystem('./templates');
 // $twig = new Twig_Environment($loader, array(
 // 		'cache' => './templates_cache',
 // ));
 
+$aBitOfInfo = function (\Slim\Route $route) {
+	echo "Current route is " . $route->getName();
+};
 
 $app->get('/', function () {
 	echo 'index request';
 });
 
-$app->get('/welcome', function () use ($app) {
+$app->get('/welcome', $aBitOfInfo, function () use ($app) {
 	echo $app->render('welcome.html', array('name' => 'shawn'));
-});
+// 	$url = $app->urlFor('home', array('args' => 'Josh'));
+// 	$app->redirect($url);
+})->name('welcome');
 
 $app->get('/home/:args', function ($args = 'a') {
 	echo 'get request ' . $args;
-});
+})->name('home');
